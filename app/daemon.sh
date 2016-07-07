d379511b62b1110cfa2cdb0f399d635a871df2fd
Dmitry <irvis@imega.ru>
2016-07-07 22:05:52 +0300

    add send notify

--- app/daemon.sh
+++ app/daemon.sh
@@ -10,9 +10,10 @@ do
         PASS=$(echo `(printf "get auth:$UUID\r\n"; sleep 0.3) | nc teleport_data 6379` | awk '{print $2}')
         DATA=$(echo `(printf "get user:$UUID\r\n"; sleep 0.3) | nc teleport_data 6379` | awk '{print $2}')
         SITE=$(echo $DATA | jq '.url' | sed 's/\"//g')
-        FILES=$(find * -type f -print)
-        URL="a.imega.ru"
-        JSON=`echo $FILES | jq -R 'split(" ")| {url:env.URL,files: .}'`
-        echo "curl -X POST -u $UUID:$PASS --data='$URLS' $SITE?mode=accept-file" >/proc/1/fd/1
+        FILES=$(find $DEST* -type f -print | sed 's|/data||g')
+        URL="a.imega.club"
+        JSON=`echo $FILES | jq -Rc --arg url "$URL" 'split(" ")| {url:$url,files: .}'`
+        curl -s -X POST -u $UUID:$PASS --data '$JSON' $SITE?mode=accept-file
+        #echo "curl -X POST -u $UUID:$PASS --data='$JSON' $SITE?mode=accept-file" >/proc/1/fd/1
     fi
 done
