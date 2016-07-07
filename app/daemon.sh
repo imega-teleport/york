b9a322b87df04a45f50df37938f7a86088ae4697
Dmitry <irvis@imega.ru>
2016-07-07 10:52:43 +0300

    add files list

--- app/daemon.sh
+++ app/daemon.sh
@@ -10,6 +10,9 @@ do
         PASS=$(echo `(printf "get auth:$UUID\r\n"; sleep 0.3) | nc teleport_data 6379` | awk '{print $2}')
         DATA=$(echo `(printf "get user:$UUID\r\n"; sleep 0.3) | nc teleport_data 6379` | awk '{print $2}')
         SITE=$(echo $DATA | jq '.url' | sed 's/\"//g')
-        echo "curl -u $UUID:$PASS $SITE" >/proc/1/fd/1
+        FILES=$(find * -type f -print)
+        URL="a.imega.ru"
+        JSON=`echo $FILES | jq -R 'split(" ")| {url:env.URL,files: .}'`
+        echo "curl -X POST -u $UUID:$PASS --data='$URLS' $SITE?mode=accept-file" >/proc/1/fd/1
     fi
 done
