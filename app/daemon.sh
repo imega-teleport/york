840113bcb443c2bc1266316941f19aa4ae631940
Dmitry <irvis@imega.ru>
2016-07-08 11:41:25 +0300

    add md5

--- app/daemon.sh
+++ app/daemon.sh
@@ -10,9 +10,11 @@ do
         PASS=$(echo `(printf "get auth:$UUID\r\n"; sleep 0.3) | nc teleport_data 6379` | awk '{print $2}')
         DATA=$(echo `(printf "get user:$UUID\r\n"; sleep 0.3) | nc teleport_data 6379` | awk '{print $2}')
         SITE=$(echo $DATA | jq '.url' | sed 's/\"//g')
-        FILES=$(find $DEST* -type f -print | sed 's|/data||g')
+        #FILES=$(find $DEST* -type f -print | sed 's|/data||g')
         URL="a.imega.club"
-        JSON=`echo $FILES | jq -Rc --arg url "$URL" 'split(" ")| {url:$url,files: .}'`
+        #JSON=`echo $FILES | jq -Rc --arg url "$URL" 'split(" ")| {url:$url,files: .}'`
+        FILES=$(find $DEST* -type f -print0 | xargs -0 md5sum | awk '{print $2":"$1}')
+        JSON=`echo $FILES | jq -Rc --arg url "$URL" 'split(" ") | {url:$url,files:[ .[]|split(":")|{(.[0]) : .[1]} ]}'`
         curl -s -X POST -u $UUID:$PASS --data '$JSON' $SITE?mode=accept-file
         echo -e `date`"\tteleport_manager: user $UUID send data='$JSON' to $SITE?mode=accept-file" >/proc/1/fd/1
     fi
