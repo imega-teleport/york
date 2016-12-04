1032e9ca09200a906cbdd181f564c11434e4140f
Dmitry <irvis@imega.ru>
2016-12-04 19:39:24 +0300

    up test

--- app/send_list.lua
+++ app/send_list.lua
@@ -2,8 +2,12 @@
 
 local i = require "inspect"
 
-local json  = require "cjson"
-local redis = require "redis"
+local json   = require "cjson"
+local redis  = require "redis"
+local base64 = require "kloss.base64"
+local curl   = require "lcurl"
+
+local teleportUrl = "http://a.imega.club"
 
 local redis_ip   = arg[1]
 local redis_port = tonumber(arg[2])
@@ -24,3 +28,43 @@ local data = json.decode(userData)
 if json.null == data then
     error("fail json decode")
 end
+
+local credentials = base64.encode(data['login'] .. ":" .. data['pass'])
+
+local data = json.decode(client:get('user:files:' .. uuid))
+if json.null == data then
+    error("fail json decode")
+end
+
+local sendData = {
+    url     = teleportUrl,
+    uuid    = uuid,
+    uripath = "storage",
+    files   = files,
+}
+
+--local ret = {}
+--local site = curl.easy()
+--:setopt_url(data['url'] .. '/teleport?mode=accept-file')
+--:setopt_httpheader{
+--    "Authorization: Basic " .. credentials,
+--    'Content-Type: application/json',
+--}
+--:setopt_postfields(json.encode(sendData))
+--:setopt_writefunction(
+--    function (response)
+--        table.insert(ret, response)
+--    end
+--)
+--
+--local perform = function ()
+--    assert(site:perform())
+--end
+--
+--if not pcall(perform) then
+--    ngx.status = ngx.HTTP_BAD_REQUEST
+--    ngx.say("400 HTTP_BAD_REQUEST")
+--    ngx.exit(ngx.status)
+--end
+--
+--site:close()
--- tests/spec/send_list_spec.lua
+++ tests/spec/send_list_spec.lua
@@ -1,9 +1,12 @@
 local i = require "inspect"
 
 describe("Отправление списка доступных файлов для скачивания", function()
-    local client, mockClient,
-          redis,  mockRedis,
-          cjson,  mockCjson
+    local client,     mockClient,
+          redis,      mockRedis,
+          cjson,      mockCjson,
+          base64,     mockBase64,
+          lcurl,      mockLcurl,
+          curlClient, mockCurlClient
 
     before_each(function()
         _G.arg = {
@@ -41,10 +44,38 @@ describe("Отправление списка доступных файлов д
             return mockCjson
         end
 
+        base64 = {
+            encode = function(value) end,
+        }
+        mockBase64 = mock.new(base64, true)
+        package.loaded['kloss.base64'] = nil
+        package.preload['kloss.base64'] = function ()
+            return mockBase64
+        end
+
+
+        curlClient = {
+            setopt_url = function(value) end,
+            setopt_httpheader = function(value) end,
+            setopt_postfields = function(value) end,
+            setopt_writefunction = function(value) end,
+        }
+        mockCurlClient = mock.new(curlClient, true)
+        lcurl = {
+            easy = function() end,
+        }
+        mockLcurl = mock.new(lcurl, true)
+        mockLcurl.easy.returns(mockCurlClient)
+        package.loaded.lcurl = nil
+        package.preload['lcurl'] = function ()
+            return mockLcurl
+        end
+
         package.loaded.send_list = nil
     end)
 
-   local userData = "{\"login\":\"9915e49a-4de1-41aa-9d7d-c9a687ec048d\",\"url\":\"mock-server\",\"email\":\"info@example.com\",\"create\":\"\",\"pass\":\"\"}"
+    local userData = "{\"login\":\"9915e49a-4de1-41aa-9d7d-c9a687ec048d\",\"url\":\"mock-server\",\"email\":\"info@example.com\",\"create\":\"\",\"pass\":\"\"}"
+    local filesData = '{"url":"a.imega.club","files":["/9915e49a-4de1-41aa-9d7d-c9a687ec048d/dump.sql"]}'
 
     it("Эталонные условия, база - ok, плагин - ok", function()
         mockClient.get.on_call_with(mockClient, "user:9915e49a-4de1-41aa-9d7d-c9a687ec048d").returns(userData)
@@ -55,6 +86,17 @@ describe("Отправление списка доступных файлов д
             create = "",
             pass   = "",
         })
+        mockBase64.encode.on_call_with("9915e49a-4de1-41aa-9d7d-c9a687ec048d:").returns("OTkxNWU0OWEtNGRlMS00MWFhLTlkN2QtYzlhNjg3ZWMwNDhkOgo=")
+        mockClient.get.on_call_with(mockClient, "user:files:9915e49a-4de1-41aa-9d7d-c9a687ec048d").returns(filesData)
+        mockCjson.decode.on_call_with(filesData).returns({
+            url   = "a.imega.club",
+            files = {
+                "/9915e49a-4de1-41aa-9d7d-c9a687ec048d/dump.sql",
+            },
+        })
+
+        mockCurlClient.setopt_url.on_call_with(mockLcurl, "a.imega.club/teleport?mode=accept-file").returns(mockCurlClient)
+        mockCurlClient.setopt_postfields.on_call_with(mockLcurl, 1).returns(mockCurlClient)
         local send_list = require 'send_list'
         assert.True(send_list)
     end)
@@ -78,6 +120,23 @@ describe("Отправление списка доступных файлов д
     it("Данные пользователя повреждены", function()
         mockClient.get.on_call_with(mockClient, "user:9915e49a-4de1-41aa-9d7d-c9a687ec048d").returns(userData)
         mockCjson.decode.on_call_with(userData).returns(nil)
+        local send_list, err = pcall(require, 'send_list')
+
+        assert.False(send_list)
+    end)
+
+    it("Данные файлов пользователя повреждены", function()
+        mockClient.get.on_call_with(mockClient, "user:9915e49a-4de1-41aa-9d7d-c9a687ec048d").returns(userData)
+        mockCjson.decode.on_call_with(userData).returns({
+            login  = "9915e49a-4de1-41aa-9d7d-c9a687ec048d",
+            url    = "mock-server",
+            email  = "info@example.com",
+            create = "",
+            pass   = "",
+        })
+        mockBase64.encode.on_call_with("9915e49a-4de1-41aa-9d7d-c9a687ec048d:").returns("OTkxNWU0OWEtNGRlMS00MWFhLTlkN2QtYzlhNjg3ZWMwNDhkOgo=")
+        mockClient.get.on_call_with(mockClient, "user:files:9915e49a-4de1-41aa-9d7d-c9a687ec048d").returns(filesData)
+        mockCjson.decode.on_call_with(filesData).returns(nil)
         local send_list, err = pcall(require, 'send_list')
 
         assert.False(send_list)
